7019b158d2fba5e423a7dd211ea92f4b587518ba
Kevin (Taeyoon) Jin <hello@kevintyj.com>
2023-11-05 09:44:05 +0530

    feat: Add flex colors uniform (#1)
    Generated using Ambient Color System

---
+++ src/dark.ts
@@ -0,0 +1,207 @@
+export const neutralDark: ColorScale<HexColor> = {
+	0: '#1b1b1c',
+	1: '#1f1f1f',
+	2: '#282827',
+	3: '#343434',
+	4: '#5b5b5b',
+	5: '#6f6f6f',
+	6: '#818181',
+	7: '#a7a7a7',
+	8: '#cfcfce',
+	9: '#f8f8f7',
+};
+
+export const oceanDark: ColorScale<HexColor> = {
+	0: '#161921',
+	1: '#141b2c',
+	2: '#152442',
+	3: '#1c345c',
+	4: '#3268af',
+	5: '#3f85d8',
+	6: '#4ca0fa',
+	7: '#89bafb',
+	8: '#b6d6fb',
+	9: '#def2fa',
+};
+
+export const blueDark: ColorScale<HexColor> = {
+	0: '#161721',
+	1: '#13162c',
+	2: '#121941',
+	3: '#192158',
+	4: '#2b3ca3',
+	5: '#364bc8',
+	6: '#435be6',
+	7: '#8687ef',
+	8: '#b7b7f7',
+	9: '#e2e8fe',
+};
+
+export const indigoDark: ColorScale<HexColor> = {
+	0: '#16171f',
+	1: '#121529',
+	2: '#13153b',
+	3: '#1c1951',
+	4: '#372796',
+	5: '#462fb9',
+	6: '#5738d6',
+	7: '#9071e4',
+	8: '#bcaaf0',
+	9: '#e4e5fb',
+};
+
+export const violetDark: ColorScale<HexColor> = {
+	0: '#19171f',
+	1: '#191428',
+	2: '#1e133b',
+	3: '#2a1752',
+	4: '#50239b',
+	5: '#652bbf',
+	6: '#7934de',
+	7: '#a670e8',
+	8: '#cba8f0',
+	9: '#ece1f7',
+};
+
+export const crimsonDark: ColorScale<HexColor> = {
+	0: '#1e1618',
+	1: '#281318',
+	2: '#3c131e',
+	3: '#541a2b',
+	4: '#9e3056',
+	5: '#c43c6f',
+	6: '#e34b88',
+	7: '#ef83a9',
+	8: '#f7b5cb',
+	9: '#fae4ee',
+};
+
+export const wineDark: ColorScale<HexColor> = {
+	0: '#1d1617',
+	1: '#241315',
+	2: '#321017',
+	3: '#42141d',
+	4: '#761f2e',
+	5: '#902739',
+	6: '#a53044',
+	7: '#c66d76',
+	8: '#e2a7ac',
+	9: '#fae1e6',
+};
+
+export const redDark: ColorScale<HexColor> = {
+	0: '#1e1617',
+	1: '#261315',
+	2: '#381117',
+	3: '#4d161c',
+	4: '#91272c',
+	5: '#b33035',
+	6: '#cf3c40',
+	7: '#e57a73',
+	8: '#f5b1aa',
+	9: '#fee7e4',
+};
+
+export const sunDark: ColorScale<HexColor> = {
+	0: '#1f1617',
+	1: '#2a1315',
+	2: '#3e1417',
+	3: '#571c1c',
+	4: '#a6332d',
+	5: '#cd4138',
+	6: '#ee5043',
+	7: '#fb8976',
+	8: '#ffbbac',
+	9: '#ffece5',
+};
+
+export const orangeDark: ColorScale<HexColor> = {
+	0: '#201617',
+	1: '#2d1615',
+	2: '#441b15',
+	3: '#5e2918',
+	4: '#b25420',
+	5: '#dc6b26',
+	6: '#fd832d',
+	7: '#ffa96c',
+	8: '#ffcda7',
+	9: '#fef2e4',
+};
+
+export const amberDark: ColorScale<HexColor> = {
+	0: '#201817',
+	1: '#2d1a15',
+	2: '#452415',
+	3: '#603719',
+	4: '#b47325',
+	5: '#dd942c',
+	6: '#feb335',
+	7: '#ffc976',
+	8: '#ffe0b0',
+	9: '#fdf7ea',
+};
+
+export const yellowDark: ColorScale<HexColor> = {
+	0: '#211917',
+	1: '#2e1d15',
+	2: '#452915',
+	3: '#603f19',
+	4: '#b18326',
+	5: '#daa72d',
+	6: '#f9c937',
+	7: '#ffd979',
+	8: '#ffe9b2',
+	9: '#fdfaea',
+};
+
+export const saplingDark: ColorScale<HexColor> = {
+	0: '#1a1c17',
+	1: '#1d2315',
+	2: '#273316',
+	3: '#39481d',
+	4: '#6f8b34',
+	5: '#8daf41',
+	6: '#a9cd50',
+	7: '#c5dd83',
+	8: '#dfecb4',
+	9: '#f7fce5',
+};
+
+export const greenDark: ColorScale<HexColor> = {
+	0: '#161b17',
+	1: '#122016',
+	2: '#0f2c19',
+	3: '#123d21',
+	4: '#1f763c',
+	5: '#28934c',
+	6: '#33ad5d',
+	7: '#78c78c',
+	8: '#b0e1bb',
+	9: '#e6faed',
+};
+
+export const auroraDark: ColorScale<HexColor> = {
+	0: '#161b1a',
+	1: '#12211d',
+	2: '#0f2e26',
+	3: '#134135',
+	4: '#1e7d67',
+	5: '#279c81',
+	6: '#31b79a',
+	7: '#79ceb7',
+	8: '#b0e4d6',
+	9: '#e4faf5',
+};
+
+export const cyanDark: ColorScale<HexColor> = {
+	0: '#161b1f',
+	1: '#132129',
+	2: '#132f3c',
+	3: '#194453',
+	4: '#2b869b',
+	5: '#36a9bf',
+	6: '#42c8dc',
+	7: '#88d9e6',
+	8: '#bbeaf1',
+	9: '#e9fbfb',
+};
---
+++ src/light.ts
@@ -0,0 +1,207 @@
+export const neutral: ColorScale<HexColor> = {
+	0: '#f8f8f6',
+	1: '#eaeae8',
+	2: '#d5d5d4',
+	3: '#babab9',
+	4: '#8d8d8c',
+	5: '#7a7a7a',
+	6: '#626262',
+	7: '#484847',
+	8: '#2f2f2d',
+	9: '#181816',
+};
+
+export const ocean: ColorScale<HexColor> = {
+	0: '#e6f5fa',
+	1: '#dbedfa',
+	2: '#c9e0fb',
+	3: '#b0d0fb',
+	4: '#80b4fb',
+	5: '#66a9fa',
+	6: '#359afa',
+	7: '#256eba',
+	8: '#16457d',
+	9: '#081f46',
+};
+
+export const blue: ColorScale<HexColor> = {
+	0: '#e9eefe',
+	1: '#dddffc',
+	2: '#cac9f9',
+	3: '#afacf5',
+	4: '#7b7ced',
+	5: '#5f68e9',
+	6: '#264fe3',
+	7: '#1a39ac',
+	8: '#102478',
+	9: '#061148',
+};
+
+export const indigo: ColorScale<HexColor> = {
+	0: '#ebedfb',
+	1: '#e0dbf8',
+	2: '#cfc1f4',
+	3: '#b69eed',
+	4: '#8763e1',
+	5: '#6f49db',
+	6: '#4624d3',
+	7: '#30199e',
+	8: '#1a0e6b',
+	9: '#0b003d',
+};
+
+export const violet: ColorScale<HexColor> = {
+	0: '#f0e9f8',
+	1: '#e7d8f6',
+	2: '#d9bff3',
+	3: '#c59dee',
+	4: '#9d62e5',
+	5: '#8a47e1',
+	6: '#6c1cdb',
+	7: '#5015a3',
+	8: '#350e6e',
+	9: '#1c063d',
+};
+
+export const crimson: ColorScale<HexColor> = {
+	0: '#faeaf2',
+	1: '#fadce8',
+	2: '#f9c7d8',
+	3: '#f6abc4',
+	4: '#ed77a1',
+	5: '#e85f93',
+	6: '#e03780',
+	7: '#a8255b',
+	8: '#731438',
+	9: '#420518',
+};
+
+export const wine: ColorScale<HexColor> = {
+	0: '#fde9ed',
+	1: '#f7d7db',
+	2: '#edbdc1',
+	3: '#de9ba0',
+	4: '#c05f68',
+	5: '#b24451',
+	6: '#9d1635',
+	7: '#770d27',
+	8: '#53051a',
+	9: '#32000a',
+};
+
+export const red: ColorScale<HexColor> = {
+	0: '#feeeec',
+	1: '#fddeda',
+	2: '#fac6c0',
+	3: '#f3a69e',
+	4: '#e26c65',
+	5: '#d9514e',
+	6: '#ca2331',
+	7: '#961524',
+	8: '#650717',
+	9: '#380004',
+};
+
+export const sun: ColorScale<HexColor> = {
+	0: '#fef2ee',
+	1: '#ffe4dc',
+	2: '#ffcec3',
+	3: '#ffb1a1',
+	4: '#f97c68',
+	5: '#f36451',
+	6: '#ea3e33',
+	7: '#af2925',
+	8: '#781617',
+	9: '#460405',
+};
+
+export const orange: ColorScale<HexColor> = {
+	0: '#fdf6ed',
+	1: '#ffebda',
+	2: '#ffdbbf',
+	3: '#ffc69b',
+	4: '#ffa05d',
+	5: '#ff8f41',
+	6: '#fb7912',
+	7: '#bf5614',
+	8: '#873512',
+	9: '#52160b',
+};
+
+export const amber: ColorScale<HexColor> = {
+	0: '#fdfaf2',
+	1: '#fff3e1',
+	2: '#ffe9c6',
+	3: '#ffdba4',
+	4: '#ffc467',
+	5: '#ffba4b',
+	6: '#fdad18',
+	7: '#c17c1a',
+	8: '#894d17',
+	9: '#542210',
+};
+
+export const yellow: ColorScale<HexColor> = {
+	0: '#fdfcf2',
+	1: '#fff7e1',
+	2: '#fff0c8',
+	3: '#ffe6a7',
+	4: '#ffd56b',
+	5: '#fdce4e',
+	6: '#f8c51a',
+	7: '#c08d1a',
+	8: '#8b5916',
+	9: '#58280f',
+};
+
+export const sapling: ColorScale<HexColor> = {
+	0: '#fafeec',
+	1: '#f3f9dd',
+	2: '#e9f2c7',
+	3: '#dae9aa',
+	4: '#bfd977',
+	5: '#b3d261',
+	6: '#a2c940',
+	7: '#78962b',
+	8: '#506618',
+	9: '#2b3a06',
+};
+
+export const green: ColorScale<HexColor> = {
+	0: '#eefdf3',
+	1: '#def6e4',
+	2: '#c6ebce',
+	3: '#a6dcb1',
+	4: '#6bc280',
+	5: '#4db66b',
+	6: '#0da750',
+	7: '#067c39',
+	8: '#015423',
+	9: '#002f0f',
+};
+
+export const aurora: ColorScale<HexColor> = {
+	0: '#ebfdf9',
+	1: '#dcf7f0',
+	2: '#c5ede2',
+	3: '#a6e0d0',
+	4: '#6bc9b0',
+	5: '#4ebfa3',
+	6: '#02b192',
+	7: '#01846c',
+	8: '#005949',
+	9: '#003228',
+};
+
+export const cyan: ColorScale<HexColor> = {
+	0: '#effdfd',
+	1: '#e2f8fa',
+	2: '#cef1f5',
+	3: '#b2e7ef',
+	4: '#7bd6e4',
+	5: '#5ecedf',
+	6: '#20c4d9',
+	7: '#148fa2',
+	8: '#085e6f',
+	9: '#01313f',
+};

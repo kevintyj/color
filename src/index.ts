f70ee021f70cd28f4bae283ad3336cdc97d82ab5
kevintyj <hello@kevintyj.com>
2023-11-05 09:05:31 +0530

    feat: Add flex colors uniform
    Generated using Ambient Color System

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export const color: ColorScale<HexColor> = {
-	white: '#ffffff',
+/* Export Ambient Color Background Colors (HEX) */
+export const BaseBackground: ColorScale<HexColor> = {
+	light: '#FFFFFF',
+	dark: '#181819',
 };
+
+/* Export Ambient Color Scales (HEX) */
+export * from './light';
+export * from './dark';

5cc2ff1efd2345a9428a10c6be88566a55a9b679
kevintyj <hello@kevintyj.com>
2023-11-21 21:40:11 +0530

    feat: Add types for flex colors uniform

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import type * as light from './light';
+import type * as dark from './dark';
+
 /* Export Ambient Color Background Colors (HEX) */
 export const BaseBackground: ColorScale<HexColor> = {
 	light: '#FFFFFF',
@@ -7,3 +10,7 @@ export const BaseBackground: ColorScale<HexColor> = {
 /* Export Ambient Color Scales (HEX) */
 export * from './light';
 export * from './dark';
+
+/* Export Ambient Color Scales Names as Types */
+export type lightColorsType = keyof typeof light;
+export type darkColorsType = keyof typeof dark;
